54780a44a1f5518ddcbae9da2e3796924efa8f50
Lubanga <lubangadenis@gmail.com>
2025-04-13 12:57:08 +0300

    Update CORS configuration to allow requests from Vite frontend
    - Added "http://localhost:5173" to the list of allowed origins for CORS in the backend configuration, enabling support for the Vite development server.

--- apps/backend/main.py
+++ apps/backend/main.py
@@ -16,7 +16,10 @@ def get_settings():
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # React frontend
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:5173",
+    ],  # React vite frontend
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
