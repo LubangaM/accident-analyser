669ddfd1d01af04618e78b2b5ef869f076081756
Lubanga <lubangadenis@gmail.com>
2025-04-13 18:17:55 +0300

    Update AccidentList component and enhance RootComponent structure
    - Modified AccidentList to pass accident.id directly as a parameter in the edit link.
    - Replaced the static greeting in RootComponent with a Header component for improved layout and user experience.

--- apps/dashboard/src/routes/__root.tsx
+++ apps/dashboard/src/routes/__root.tsx
@@ -1,15 +1,16 @@
-import * as React from 'react'
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import * as React from "react";
+import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { Header } from "../components/Header";
 
 export const Route = createRootRoute({
   component: RootComponent,
-})
+});
 
 function RootComponent() {
   return (
     <React.Fragment>
-      <div>Hello "__root"!</div>
+      <Header />
       <Outlet />
     </React.Fragment>
-  )
+  );
 }
